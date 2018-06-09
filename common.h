2aa8936dcd3e43e1d3e1fcbf2210caf0d13c611c
pandawang <40022676+pangdawang@users.noreply.github.com>
2018-06-09 10:10:59 +0800

    Update common.h
    去掉代码注释

--- common.h
+++ common.h
@@ -13,18 +13,7 @@
 //#include "text.h"		
 #include "usart.h"	  
 //#include "ff.h"
-/////////////////////////////////////////////////////////////////////////////////////////////////////////// 
-//本程序只供学习使用，未经作者许可，不得用于其它任何用途
-//ALIENTEK STM32开发板
-//ATK-ESP8266 WIFI模块 公用驱动代码	   
-//正点原子@ALIENTEK
-//技术论坛:www.openedv.com
-//修改日期:2014/4/3
-//版本：V1.0
-//版权所有，盗版必究。
-//Copyright(C) 广州市星翼电子科技有限公司 2009-2019
-//All rights reserved									  
-/////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+
 	
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////// 
@@ -53,20 +42,20 @@ void atk_8266_test(void);
 
 
 
-//u8 atk_8266_apsta_test(void);	//WIFI AP+STA模式测试
-//u8 atk_8266_wifista_test(void);	//WIFI STA测试
-///u8 atk_8266_wifiap_test(void);	//WIFI AP测试
+//u8 atk_8266_apsta_test(void);	//WIFI AP+STA妯″紡娴嬭瘯
+//u8 atk_8266_wifista_test(void);	//WIFI STA娴嬭瘯
+///u8 atk_8266_wifiap_test(void);	//WIFI AP娴嬭瘯
 
-//用户配置参数
-extern const u8* portnum;			//连接端口
+//鐢ㄦ埛閰嶇疆鍙傛暟
+extern const u8* portnum;			//杩炴帴绔彛
  
 extern const u8* wifista_ssid;		//WIFI STA SSID
-extern const u8* wifista_encryption;//WIFI STA 加密方式
-extern const u8* wifista_password; 	//WIFI STA 密码
+extern const u8* wifista_encryption;//WIFI STA 鍔犲瘑鏂瑰紡
+extern const u8* wifista_password; 	//WIFI STA 瀵嗙爜
 
 extern const u8* wifiap_ssid;		//WIFI AP SSID
-extern const u8* wifiap_encryption;	//WIFI AP 加密方式
-extern const u8* wifiap_password; 	//WIFI AP 密码
+extern const u8* wifiap_encryption;	//WIFI AP 鍔犲瘑鏂瑰紡
+extern const u8* wifiap_password; 	//WIFI AP 瀵嗙爜
 
 extern u8* kbd_fn_tbl[2];
 extern const u8* ATK_ESP8266_CWMODE_TBL[3];
